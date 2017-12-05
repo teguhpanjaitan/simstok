c406327cbfe4b948bd5920ad0b1caf0026e8a7f1
teguh panjaitan <teguh panjaitan>
2017-12-05 08:14:18 +0700

    Theme

---
+++ module/Front/src/Module.php
@@ -0,0 +1,12 @@
+<?php
+namespace Front;
+
+class Module
+{
+    const VERSION = '3.0.3-dev';
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/../config/module.config.php';
+    }
+}
